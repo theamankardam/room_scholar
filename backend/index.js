cc7fafa70d406b8c3bc4847eb45c0499c3db3877
Aman Kardam <amankardam54@gmailc.om>
2026-05-19 11:02:33 +0530

    vercel configure

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const app = express();
-const db = require("./src/db/db.js")
+const db = require("./src/db/db.js");
 require("dotenv").config();
 const propertyRoutes = require("./src/routes/propertyRoutes.js");
-const cors = require('cors')
-
-
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
+// DB connect
+db();
+
+// routes
 app.use("/api/properties", propertyRoutes);
 
 
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.get("/", (req, res) => {
+  res.status(200).send("Backend API is running 🚀");
 });
+
+
+// ❌ NO app.listen here (IMPORTANT)
+module.exports = app;
