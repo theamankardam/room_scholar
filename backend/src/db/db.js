e35ca8c1ada7d459bbcda3b62ae11b2e878753ec
Aman Kardam <amankardam54@gmailc.om>
2026-05-19 09:53:02 +0530

    backend configure

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,12 +1,15 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
-const mongoURL = process.env.MONGODB_URL;
 
-mongoose
-  .connect(mongoURL)
-  .then(() => {
-    console.log("MongoDB Connected...");
-  })
-  .catch((err) => {
-    console.log(`MongoDB Connection Error`);
-  });
+const connectDB = async () => {
+  try {
+    if (mongoose.connections[0].readyState) return;
+
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log("DB Error:", err.message);
+  }
+};
+
+module.exports = connectDB;
